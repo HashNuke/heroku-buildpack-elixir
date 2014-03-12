97c7f2e2a8ac02a6d5a347e7c6a70554d1622e67
Akash Manohar J <akash@akash.im>
2014-03-12 12:36:32 +0530

    Fix building erlang

--- lib/erlang_funcs.sh
+++ lib/erlang_funcs.sh
@@ -6,12 +6,14 @@ function erlang_tarball() {
 function download_erlang() {
   local erlang_package_url="https://s3.amazonaws.com/heroku-buildpack-erlang/$(erlang_tarball)"
 
+  output_line "Checking if Erlang has already been downloaded..."
   exit_if_file_exists ${cache_path}/$(erlang_tarball)
+  output_line "Downloading Erlang package"
 
   # Set this so that rebar and elixir will be force-rebuilt
   erlang_changed=true
 
-  # Delete previously downloaded OTP tar files
+  # Delete previously downloaded OTP tarballs
   rm -rf ${cache_path}/OTP_*.tgz
 
   cd ${cache_path}
@@ -23,7 +25,7 @@ function download_erlang() {
 
 function install_erlang() {
   output_section "Installing Erlang ${erlang_version}"
-  local $absolute_erlang_path=/app/.platform_tools/erlang
+  local $absolute_erlang_path="/app/.platform_tools/erlang"
 
   rm -rf ${erlang_build_path}
   mkdir -p ${erlang_build_path}
