f4cb7ef639ce5284c8fad6bc59fd090d91ee612b
Akash Manohar J <akash@akash.im>
2014-04-09 13:45:27 +0530

    Debugging lines

--- lib/erlang_funcs.sh
+++ lib/erlang_funcs.sh
@@ -34,6 +34,8 @@ function download_erlang() {
     output_section "Fetching Erlang ${erlang_version[0]} ${erlang_version[1]}"
     echo ${erlang_package_url}
     curl -ksO ${erlang_package_url} -o $(erlang_tarball) || exit 1
+    echo "After downloading erlang..."
+    ls
     cd - > /dev/null
   else
     output_section "[skip] Already downloaded Erlang ${erlang_version[0]} ${erlang_version[1]}"
