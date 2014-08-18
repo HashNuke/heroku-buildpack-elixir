7a623fd71292db61c2131cd5f905036b5efc78f0
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-08-18 16:39:37 +0200

    Fix "unary operator expected"

--- lib/elixir_funcs.sh
+++ lib/elixir_funcs.sh
@@ -38,6 +38,8 @@ function fix_elixir_version() {
     elixir_version=${elixir_version[1]}
 
   elif [ ${#elixir_version[@]} -eq 1 ]; then
+    force_fetch=false
+
     # If we detect a version string (ex: 0.15.1) we prefix it with "v"
     if [[ ${elixir_version} =~ ^[0-9]+\.[0-9]+\.[0-9]+ ]]; then
       elixir_version=v${elixir_version}
