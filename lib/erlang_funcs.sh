827ca27b709fd320acce3a6ac99d47e13869491e
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-05-13 04:07:35 +0200

    Use new path for erlang builds

--- lib/erlang_funcs.sh
+++ lib/erlang_funcs.sh
@@ -4,12 +4,7 @@ function erlang_tarball() {
 
 
 function download_erlang() {
-  if [ ${STACK} = "cedar-14" ] ; then
-    erlang_package_url="https://s3.amazonaws.com/s3.hex.pm/builds/erlang/ubuntu-14.04"
-  else
-    erlang_package_url="https://s3.amazonaws.com/s3.hex.pm/builds/erlang/ubuntu-10.04"
-  fi
-
+  erlang_package_url="https://s3.amazonaws.com/s3.hex.pm/builds/erlang/cedar-14"
   erlang_package_url="${erlang_package_url}/$(erlang_tarball)"
 
   # If a previous download does not exist, then always re-download
