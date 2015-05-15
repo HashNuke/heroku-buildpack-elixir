0825a1f6b998aacc5ae73504d45b23979f7b4e3c
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-05-15 23:19:50 +0200

    Print if elixir or erlang changed

--- lib/erlang_funcs.sh
+++ lib/erlang_funcs.sh
@@ -2,7 +2,6 @@ function erlang_tarball() {
   echo "OTP-${erlang_version}.tar.gz"
 }
 
-
 function download_erlang() {
   erlang_package_url="https://s3.amazonaws.com/s3.hex.pm/builds/erlang/cedar-14"
   erlang_package_url="${erlang_package_url}/$(erlang_tarball)"
@@ -17,18 +16,16 @@ function download_erlang() {
     output_section "Fetching Erlang ${erlang_version}"
     curl -s ${erlang_package_url} -o ${cache_path}/$(erlang_tarball) || exit 1
   else
-    output_section "[skip] Already downloaded Erlang ${erlang_version}"
+    output_section "Using cached Erlang ${erlang_version}"
   fi
 }
-
 
 function clean_erlang_downloads() {
   rm -rf ${cache_path}/OTP-*.tar.gz
 }
 
-
 function install_erlang() {
-  output_section "Installing Erlang ${erlang_version}"
+  output_section "Installing Erlang ${erlang_version} $(erlang_changed)"
 
   rm -rf $(erlang_build_path)
   mkdir -p $(erlang_build_path)
@@ -41,4 +38,10 @@ function install_erlang() {
 
   cp -R $(erlang_build_path) $(erlang_path)
   PATH=$(erlang_path)/bin:$PATH
+}
+
+function erlang_changed() {
+  if [ $erlang_changed = true ]; then
+    echo "(changed)"
+  fi
 }
