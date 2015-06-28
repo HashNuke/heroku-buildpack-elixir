a7a967b2ca47b1d2c4fc1fe52313eb3d1c7365b1
Akash Manohar J <akash@akash.im>
2015-06-28 14:50:40 +0530

    Use port from env var

--- tests/hello_plug/lib/hello_plug/server.ex
+++ tests/hello_plug/lib/hello_plug/server.ex
@@ -1,5 +1,8 @@
 defmodule HelloPlug.Server do
   def start_link do
-    Plug.Adapters.Cowboy.http HelloPlug.App, []
+    port = ( System.get_env("PORT") || "4000" )
+    |> String.to_integer
+
+    Plug.Adapters.Cowboy.http HelloPlug.App, [], port: port
   end
 end
