6451cec10bcf0d2d8994be707dfb69ecf1144cf9
Akash Manohar J <akash@akash.im>
2015-10-23 13:25:24 +0530

    switch back to static cedar-14 string

--- lib/erlang_funcs.sh
+++ lib/erlang_funcs.sh
@@ -3,7 +3,7 @@ function erlang_tarball() {
 }
 
 function download_erlang() {
-  erlang_package_url="https://s3.amazonaws.com/heroku-buildpack-elixir/erlang/$STACK"
+  erlang_package_url="https://s3.amazonaws.com/heroku-buildpack-elixir/erlang/cedar-14"
   erlang_package_url="${erlang_package_url}/$(erlang_tarball)"
 
   # If a previous download does not exist, then always re-download
