4f34eaed1131e032e1bfebdcbd52e9405e54cccb
Jesse <jesse@gigalixir.com>
2020-12-10 10:15:06 -0800

    explicitly define the memoized variable as global.

--- lib/path_funcs.sh
+++ lib/path_funcs.sh
@@ -20,7 +20,7 @@ function elixir_path() {
 
 function erlang_build_path() {
   if [ -z "$tmp_erlang_build_dir" ]; then
-    tmp_erlang_build_dir=$(mktemp -d)
+    global tmp_erlang_build_dir=$(mktemp -d)
   fi
   echo "${tmp_erlang_build_dir}/erlang"
 }
